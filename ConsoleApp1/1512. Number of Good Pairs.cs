44876ffa368152c9f00a95937d3cfc8b267d31e9
tankw <tantk113@gmail.com>
2023-10-03 12:43:16 +0700

    Add project files.

---
+++ ConsoleApp1/1512. Number of Good Pairs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class1
+    {
+        public int NumIdenticalPairs(int[] nums)
+        {
+            int count = 0;
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[j] == nums[i])
+                        count++;
+
+                }
+
+            }
+            return count;
+
+        }
+    }
+}
